a2f10f625bbe2fd4c62aab5e9c20f91949d1b460
George Magdy <georgtriple@gmail.com>
2022-03-15 18:39:27 +0200

    Add write_file method for music album

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -11,4 +11,22 @@ class MusicAlbum < Item
   def can_be_archived?
     super.can_be_archived? && on_spotify == true
   end
+
+  def self.path
+    'files/music_album.json'
+  end
+
+  def self.write_file(data = [])
+    data_arr = []
+    data.each do |d|
+      data_arr << { 
+        id: d.id, 
+        publish_date: d.publish_date, 
+        archived: d.archived
+        on_spotify: d.on_spotify,
+        genre_id: d.genre.id
+      }
+    end
+    File.write(MusicAlbum.path, JSON.generate(data_arr))
+  end
 end
