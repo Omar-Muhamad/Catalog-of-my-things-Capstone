df846cf6072cd2c84ebf8e0c42265dc0170c97c0
George Magdy <georgtriple@gmail.com>
2022-03-15 10:33:58 +0200

    Refactor genre intializer

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,8 +1,8 @@
 class Genre
   attr_reader :id, :name, :items
 
-  def initialize(name)
-    @id = Random.rand(1..1000)
+  def initialize(id, name)
+    @id = id
     @name = name
     @items = []
   end
