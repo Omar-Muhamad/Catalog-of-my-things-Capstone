37f9007e7673adb3e06bb4c580d79d92c52ab5a3
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-03-16 15:13:52 +0200

    Add Tests to book class

--- classes/book.rb
+++ classes/book.rb
@@ -3,14 +3,14 @@ require_relative 'item'
 class Book < Item
   attr_accessor :title, :publisher, :cover_state
 
-  def initialize(title, publisher, cover_state)
-    super(id, publish_date)
+  def initialize(title, publisher, cover_state, *args, **kwargs)
+    super(*args, **kwargs)
     @title = title
     @publisher = publisher
     @cover_state = cover_state
   end
 
-  def can_be_archived
-    super || cover_state == 'bad'
+  def can_be_archived?
+    super && cover_state != 'bad'
   end
 end
---
+++ spec/book_spec.rb
@@ -0,0 +1,30 @@
+require_relative '../classes/book'
+
+describe 'Create book object' do
+  context 'given the valid inputs' do
+    it 'should create a person with a title Harry Potter' do
+      book = Book.new("harry potter", "omar", "good", 2000, true)
+      expect(book.title).to eql('harry potter')
+    end
+    it 'should create a person with a publisher omar' do
+      book = Book.new("harry potter", "omar", "good", 2000, true)
+      expect(book.publisher).to eql('omar')
+    end
+    it 'should create a person with a title Harry Potter' do
+      book = Book.new("harry potter", "omar", "good", 2000, true)
+      expect(book.cover_state).to eql('good')
+    end
+    it 'can_be_archived? should return true if the book archivable and Cover_state is good' do
+      book = Book.new("harry potter", "omar", "good", 2000, true)
+      expect(book.can_be_archived?).to be(true)
+    end
+    it 'can_be_archived? should return false if the book not archivable and Cover_state is good' do
+      book = Book.new("harry potter", "omar", "good", 2020, true)
+      expect(book.can_be_archived?).to be(false)
+    end
+    it 'can_be_archived? should return false if the book archivable and Cover_state is bad' do
+      book = Book.new("harry potter", "omar", "bad", 2000, true)
+      expect(book.can_be_archived?).to be(false)
+    end
+  end
+end
